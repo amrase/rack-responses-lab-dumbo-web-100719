ecade4be1aea5d5a176e773f5c16daacf0563dfb
Amra Sezairi <amra.sezairi@hotmail.com>
2019-10-28 03:01:05 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -2,7 +2,7 @@ class Application
   def call(env)
     resp = Rack::Response.new 
     
-    if Time.now.hour 
+    if Time.now.hour.beetween
       resp.write "Good Morning!"
     else
       resp.write "Good Afternoon!"
